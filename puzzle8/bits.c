64681af18152b3cd21ed9296be25a16c4009ba9f
gitjoop <j.pascha@gmail.com>
2014-09-08 13:45:30 +0200

    blah

--- puzzle8/bits.c
+++ puzzle8/bits.c
@@ -12,7 +12,7 @@
  * it's not good practice to ignore compiler warnings, but in this
  * case it's OK.
  */
-
+#include <stdio.h> //todo: comment out in final version
 #if 0
 /*
  * Instructions to Students:
@@ -25,141 +25,140 @@ editing the collection of functions in this source file.
 
 INTEGER CODING RULES:
 
-  Replace the "return" statement in each function with one
-  or more lines of C code that implements the function. Your code
-  must conform to the following style:
+Replace the "return" statement in each function with one
+or more lines of C code that implements the function. Your code
+must conform to the following style:
 
-  int Funct(arg1, arg2, ...) {
-      /* brief description of how your implementation works */
-      int var1 = Expr1;
-      ...
-      int varM = ExprM;
+int Funct(arg1, arg2, ...) {
+  /* brief description of how your implementation works */
+  int var1 = Expr1;
+  ...
+    int varM = ExprM;
 
-      varJ = ExprJ;
-      ...
-      varN = ExprN;
-      return ExprR;
-  }
+  varJ = ExprJ;
+  ...
+    varN = ExprN;
+  return ExprR;
+}
 
-  Each "Expr" is an expression using ONLY the following:
-  1. Integer constants 0 through 255 (0xFF), inclusive. You are
-      not allowed to use big constants such as 0xffffffff.
-  2. Function arguments and local variables (no global variables).
-  3. Unary integer operations ! ~
-  4. Binary integer operations & ^ | + << >>
+Each "Expr" is an expression using ONLY the following:
+1. Integer constants 0 through 255 (0xFF), inclusive. You are
+not allowed to use big constants such as 0xffffffff.
+2. Function arguments and local variables (no global variables).
+3. Unary integer operations ! ~
+4. Binary integer operations & ^ | + << >>
 
-  Some of the problems restrict the set of allowed operators even further.
-  Each "Expr" may consist of multiple operators. You are not restricted to
-  one operator per line.
+Some of the problems restrict the set of allowed operators even further.
+Each "Expr" may consist of multiple operators. You are not restricted to
+one operator per line.
 
-  You are expressly forbidden to:
-  1. Use any control constructs such as if, do, while, for, switch, etc.
-  2. Define or use any macros.
-  3. Define any additional functions in this file.
-  4. Call any functions.
-  5. Use any other operations, such as &&, ||, -, or ?:
-  6. Use any form of casting.
-  7. Use any data type other than int.  This implies that you
-     cannot use arrays, structs, or unions.
+You are expressly forbidden to:
+1. Use any control constructs such as if, do, while, for, switch, etc.
+2. Define or use any macros.
+3. Define any additional functions in this file.
+4. Call any functions.
+5. Use any other operations, such as &&, ||, -, or ?:
+6. Use any form of casting.
+7. Use any data type other than int.  This implies that you
+cannot use arrays, structs, or unions.
 
 
-  You may assume that your machine:
-  1. Uses 2s complement, 32-bit representations of integers.
-  2. Performs right shifts arithmetically.
-  3. Has unpredictable behavior when shifting an integer by more
-     than the word size.
+You may assume that your machine:
+1. Uses 2s complement, 32-bit representations of integers.
+2. Performs right shifts arithmetically.
+3. Has unpredictable behavior when shifting an integer by more
+than the word size.
 
 EXAMPLES OF ACCEPTABLE CODING STYLE:
-  /*
-   * pow2plus1 - returns 2^x + 1, where 0 <= x <= 31
-   */
-  int pow2plus1(int x) {
-     /* exploit ability of shifts to compute powers of 2 */
-     return (1 << x) + 1;
-  }
+/*
+ * pow2plus1 - returns 2^x + 1, where 0 <= x <= 31
+ */
+int pow2plus1(int x) {
+  /* exploit ability of shifts to compute powers of 2 */
+  return (1 << x) + 1;
+}
 
-  /*
-   * pow2plus4 - returns 2^x + 4, where 0 <= x <= 31
-   */
-  int pow2plus4(int x) {
-     /* exploit ability of shifts to compute powers of 2 */
-     int result = (1 << x);
-     result += 4;
-     return result;
-  }
+/*
+ * pow2plus4 - returns 2^x + 4, where 0 <= x <= 31
+ */
+int pow2plus4(int x) {
+  /* exploit ability of shifts to compute powers of 2 */
+  int result = (1 << x);
+  result += 4;
+  return result;
+}
 
 FLOATING POINT CODING RULES
 
 For the problems that require you to implent floating-point operations,
-the coding rules are less strict.  You are allowed to use looping and
-conditional control.  You are allowed to use both ints and unsigneds.
-You can use arbitrary integer and unsigned constants.
+    the coding rules are less strict.  You are allowed to use looping and
+    conditional control.  You are allowed to use both ints and unsigneds.
+    You can use arbitrary integer and unsigned constants.
 
-You are expressly forbidden to:
-  1. Define or use any macros.
-  2. Define any additional functions in this file.
-  3. Call any functions.
-  4. Use any form of casting.
-  5. Use any data type other than int or unsigned.  This means that you
-     cannot use arrays, structs, or unions.
-  6. Use any floating point data types, operations, or constants.
+    You are expressly forbidden to:
+    1. Define or use any macros.
+    2. Define any additional functions in this file.
+    3. Call any functions.
+    4. Use any form of casting.
+    5. Use any data type other than int or unsigned.  This means that you
+    cannot use arrays, structs, or unions.
+    6. Use any floating point data types, operations, or constants.
 
 
-NOTES:
-  1. Use the dlc (data lab checker) compiler (described in the handout) to
-     check the legality of your solutions.
-  2. Each function has a maximum number of operators (! ~ & ^ | + << >>)
-     that you are allowed to use for your implementation of the function.
-     The max operator count is checked by dlc. Note that '=' is not
-     counted; you may use as many of these as you want without penalty.
+    NOTES:
+    1. Use the dlc (data lab checker) compiler (described in the handout) to
+  check the legality of your solutions.
+2. Each function has a maximum number of operators (! ~ & ^ | + << >>)
+  that you are allowed to use for your implementation of the function.
+  The max operator count is checked by dlc. Note that '=' is not
+  counted; you may use as many of these as you want without penalty.
   3. Use the btest test harness to check your functions for correctness.
   4. Use the BDD checker to formally verify your functions
   5. The maximum number of ops for each function is given in the
-     header comment for each function. If there are any inconsistencies
-     between the maximum ops in the writeup and in this file, consider
-     this file the authoritative source.
+  header comment for each function. If there are any inconsistencies
+  between the maximum ops in the writeup and in this file, consider
+  this file the authoritative source.
 
-/*
- * STEP 2: Modify the following functions according the coding rules.
- *
- *   IMPORTANT. TO AVOID GRADING SURPRISES:
- *   1. Use the dlc compiler to check that your solutions conform
- *      to the coding rules.
- *   2. Use the BDD checker to formally verify that your solutions produce
- *      the correct answers.
- */
+  /*
+   * STEP 2: Modify the following functions according the coding rules.
+   *
+   *   IMPORTANT. TO AVOID GRADING SURPRISES:
+   *   1. Use the dlc compiler to check that your solutions conform
+   *      to the coding rules.
+   *   2. Use the BDD checker to formally verify that your solutions produce
+   *      the correct answers.
+   */
 
 
 #endif
 
-/*
- * CS:APP Data Lab @ Universiteit van Amsterdam
- *
- * Modification to original Data Lab:
- *
- * the collection of puzzles is automatically generated for each
- * programming pair.
- *
- * Augustus 2011: A.Visser@uva.nl
- *
- */
+  /*
+   * CS:APP Data Lab @ Universiteit van Amsterdam
+   *
+   * Modification to original Data Lab:
+   *
+   * the collection of puzzles is automatically generated for each
+   * programming pair.
+   *
+   * Augustus 2011: A.Visser@uva.nl
+   *
+   */
 
 
 
-/* Global ID to identify the combination of puzzles */
-int lab_id = 8;
-/*
- * bitAnd - x&y using only ~ and |
- *   Example: bitAnd(6, 5) = 4
- *   Legal ops: ~ |
- *   Max ops: 8
- *   Rating: 1
- */
-int bitAnd(int x, int y) {
-  int total = ~x | ~y;
-  int result = ~total;
-  return result;
-}
+  /* Global ID to identify the combination of puzzles */
+  int lab_id = 8;
+  /*
+   * bitAnd - x&y using only ~ and |
+   *   Example: bitAnd(6, 5) = 4
+   *   Legal ops: ~ |
+   *   Max ops: 8
+   *   Rating: 1
+   */
+  int bitAnd(int x, int y) {
+    return ~(~x | ~y);
+  }
+
 /*
  * fitsBits - return 1 if x can be represented as an
  *  n-bit, two's complement integer.
@@ -170,8 +169,11 @@ int bitAnd(int x, int y) {
  *   Rating: 2
  */
 int fitsBits(int x, int n) {
-
+  int mask = x >> 31;
+  return !(((~x & mask) + (x & ~mask)) >> (n + ~0));
 }
+
+
 /*
  * isLessOrEqual - if x <= y  then return 1, else return 0
  *   Example: isLessOrEqual(4,5) = 1.
@@ -204,7 +206,7 @@ int ilog2(int x) {
  *   Rating: 2
  */
 unsigned float_neg(unsigned uf) {
- return 2;
+  return 2;
 }
 /*
  * float_twice - Return bit-level equivalent of expression 2*f for
